9b3d9031be5588573d418caf7dcc0f99443dc5f2
System Ato <cornerhipster@gmail.com>
2019-10-22 18:43:29 +0800

    a

--- dbModel.py
+++ dbModel.py
@@ -2,16 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from flask_sqlalchemy import create_engine
-#from sqlalchemy import create_engine
+import psycopg2
 
 
 
 app = Flask(__name__)
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'postgres://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-db_string = "postgres://vtdymwiimcircz:ab0ffa3f42eda4ecb25df6a83514ca51b4fa08c69bd31e011680361b525cc54b@ec2-54-83-52-191.compute-1.amazonaws.com:5432/d268qlch00j1lt"
-db1 = create_engine(db_string)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -20,8 +17,24 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 def getComplains():
-    result_set = db1.execute("SELECT * FROM complains;")
-    return result_set
+    conn = psycopg2.connect(database="testdb", user="postgres", password="pass123", host="127.0.0.1", port="5432")
+    print "Opened database successfully"
+
+    ur = conn.cursor()
+
+    cur.execute("SELECT *  from complains;")
+    rows = cur.fetchall()
+    """
+    for row in rows:
+    print "ID = ", row[0]
+    print "NAME = ", row[1]
+    print "ADDRESS = ", row[2]
+    print "SALARY = ", row[3], "
+    """
+
+    print "Operation done successfully"
+    conn.close()
+    return rows
 
 class UserData(db.Model):
     __tablename__ = 'UserData'
