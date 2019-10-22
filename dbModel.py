4ab632fe362c252b5e36c69892d88d73afcb80bb
System Ato <cornerhipster@gmail.com>
2019-10-22 18:46:01 +0800

    s

--- dbModel.py
+++ dbModel.py
@@ -17,8 +17,8 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 def getComplains():
-    conn = psycopg2.connect(database="testdb", user="postgres", password="pass123", host="127.0.0.1", port="5432")
-    print "Opened database successfully"
+    conn = psycopg2.connect(database="d268qlch00j1lt", user="vtdymwiimcircz", password="ab0ffa3f42eda4ecb25df6a83514ca51b4fa08c69bd31e011680361b525cc54b", host="ec2-54-83-52-191.compute-1.amazonaws.com", port="5432")
+    print ("Opened database successfully")
 
     ur = conn.cursor()
 
@@ -32,7 +32,7 @@ def getComplains():
     print "SALARY = ", row[3], "
     """
 
-    print "Operation done successfully"
+    print ("Operation done successfully")
     conn.close()
     return rows
 
