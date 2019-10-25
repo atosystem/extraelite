d37fc00bbe6f21c5729d0744299e06d4eec84204
System Ato <cornerhipster@gmail.com>
2019-10-26 00:40:33 +0800

    a

--- app_run.py
+++ app_run.py
@@ -2,6 +2,9 @@ from flask import *
 from datetime import datetime
 from dbModel import *
 import json
+import jieba
+import codecs
+import jieba.analyse
 app = Flask(__name__)
 
 
@@ -26,22 +29,25 @@ def index():
 
 @app.route('/test')
 def test():
-    """
-    data = "Deploying a Flask App To Heroku"
-    
-    data_UserData = UserData.query.all()
-    history_dic = {}
-    history_list = []
-    for _data in data_UserData:
-        history_dic['Name'] = _data.Name
-        history_dic['Id'] = _data.Id
-        history_dic['Description'] = _data.Description
-        history_dic['CreateDate'] = _data.CreateDate.strftime('%Y/%m/%d %H:%M:%S')
-        history_list.append(history_dic)
-        history_dic = {}
-    """
     
     return json.dumps(getComplains())
+
+@app.route('/testj')
+def testj():
+    print("斷詞中...")
+    jieba.load_userdict("userdict.txt")
+    jieba.analyse.set_stop_words("stopword.txt")
+    hash = {}
+    for paragraph in getComplains():
+        #print(paragraph["complain_content"])
+        seg_list = jieba.analyse.extract_tags(paragraph["complain_content"])#jieba.cut(paragraph["complain_content"],cut_all=False)
+        for word in seg_list:
+            if word in hash:
+                hash[word]+=1
+            else:
+                hash[word] = 1
+                print(word)
+    return hash
 
 
 @app.route('/API/add_data', methods=['POST'])
